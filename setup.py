e376c48a641a37d51b9c632393880933dde81248
Chris Lalancette <clalancette@openrobotics.org>
2022-11-02 18:27:34 +0000

    Rebase from 'debian/rolling/rqt_action'

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 package_name = 'rqt_action'
 setup(
     name=package_name,
-    version='2.1.0',
+    version='2.1.1',
     package_dir={'': 'src'},
     packages=[package_name],
     data_files=[
@@ -25,12 +25,10 @@ setup(
         'Topic :: Software Development',
     ],
     description=(
-        'rqt_action provides a feature to introspect all available ROS action '
-        'types. By utilizing rqt_msg, the output format is unified with it and rqt_srv. Note that '
-        'the actions shown on this plugin is the ones that are stored on your machine, not on the '
-        'ROS core your rqt instance connects to.'
+        'rqt_action provides a feature to introspect all available ROS action types.'
     ),
     license='BSD',
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'rqt_action = ' + package_name + '.main:main',
