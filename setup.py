87d6c9c6fc642d3b5395a200af6f1d9962622a40
Scott K Logan <logans@cottsay.net>
2025-04-28 16:55:08 -0500

    Rebase from 'rpm/rolling/rqt_action'

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 package_name = 'rqt_action'
 setup(
     name=package_name,
-    version='2.3.0',
+    version='2.4.0',
     package_dir={'': 'src'},
     packages=[package_name],
     data_files=[
